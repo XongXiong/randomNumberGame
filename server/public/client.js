24f41d56378d6b629c7fffa55718124053e90529
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-12 17:08:55 -0500

    gourp added stuff

--- server/public/client.js
+++ server/public/client.js
@@ -3,7 +3,16 @@ $(document).ready(readyNow);
 function readyNow(){
     console.log('script and jquery sourced');
     appendSetUp();
+    clickHandler();
 };
+
+function clickHandler(){
+    $('#container').on('click', '#start', startGame);
+    $('#container').on('click', '#easy', easyMode);
+    $('#container').on('click', '#medium', mediumMode);
+    $('#container').on('click', '#hard', hardMode);
+    // TO-DO: create handlers for play mode
+}
 
 function appendSetUp(){
 
@@ -14,14 +23,12 @@ var $div = $('<div></div>');
     $div.append('<button id="hard">Hard</button>');
     $div.append('<button id="start">Start Game</button>');
     $('#container').append($div);
-    $('#container').on('click', '#start', startGame);
-    $('#container').on('click', '#easy', easyMode);
-    $('#container').on('click', '#medium', mediumMode);
-    $('#container').on('click', '#hard', hardMode);
+   
 };
 
 function startGame(){
     console.log('start game')
+    appendPlay();
 }
 
 function easyMode() { 
@@ -39,7 +46,20 @@ function hardMode() {
     
 }
 
-
+function appendPlay(){
+    $('#container').empty();
+    var $playerDiv;
+    for (var i = 1; i < 5; i += 1){
+        $playerDiv = $('<div id="player' + i + '"></div>');
+        $playerDiv.append('<input type="text" placeholder="Player ' + i + ' guess" id="input'+ i + '">');
+        $playerDiv.append('<div id="hint' + i + '">hint</div>');
+        $('#container').append($playerDiv);
+    }
+    $('#container').append('<div>Round <span id="counter">0</span></div>');
+    $('#container').append('<button id="submit">Submit Guesses</button>');
+    $('#container').append('<div>Max Number: <span id="range">0</span></div>');
+    $('#container').append('<button id="cancel">Cancel Game</button>');
+}
 
 
 
