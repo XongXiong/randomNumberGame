507931c8406d3ef0327a54706a8b8b4482e1679a
Alexander Bliss <alexanderbliss@Alexanders-MacBook-Pro.local>
2017-10-13 10:59:08 -0500

    TOO MUCH between commit. Some routes, difficulty levels

--- server/public/client.js
+++ server/public/client.js
@@ -7,43 +7,58 @@ function readyNow(){
 };
 
 function clickHandler(){
+    // Setup handlers
     $('#container').on('click', '#start', startGame);
     $('#container').on('click', '#easy', easyMode);
     $('#container').on('click', '#medium', mediumMode);
     $('#container').on('click', '#hard', hardMode);
-    // TO-DO: create handlers for play mode
+    // Play mode handlers
+    $('#container').on('click', '#submit', submitGuess);
+    $('#container').on('click', '#cancel', appendSetUp);
 }
 
 function appendSetUp(){
 
 var $div = $('<div></div>');
     $('#container').empty();
-    $div.append('<button id="easy">Easy</button>');
-    $div.append('<button id="medium">Medium</button>');
-    $div.append('<button id="hard">Hard</button>');
+    $div.append('<button id="easy">Easy: 10</button>');
+    $div.append('<button id="medium">Medium: 50</button>');
+    $div.append('<button id="hard">Hard: 100</button>');
     $div.append('<button id="start">Start Game</button>');
     $('#container').append($div);
    
 };
 
+var maxVal = 10; 
+
 function startGame(){
-    console.log('start game')
+    console.log('start game');
     appendPlay();
+    $.ajax({
+        method: 'POST',
+        url: '/maxVal',
+        data: {maxVal: maxVal}
+    }).done(function(response){
+        console.log("maxVal POST:",response);        
+    }).fail(function(message){
+        console.log('error!', message);
+        
+    })
 }
 
 function easyMode() { 
     console.log('easy game');
-    
+    maxVal = 10;
 }
 
 function mediumMode() { 
     console.log('medium game');
-    
+    maxVal = 50;
 }
 
 function hardMode() { 
     console.log('hard game');
-    
+    maxVal = 100;
 }
 
 function appendPlay(){
@@ -57,10 +72,40 @@ function appendPlay(){
     }
     $('#container').append('<div>Round <span id="counter">0</span></div>');
     $('#container').append('<button id="submit">Submit Guesses</button>');
-    $('#container').append('<div>Max Number: <span id="range">0</span></div>');
+    $('#container').append('<div>Max Number: ' + maxVal + '</div>');
     $('#container').append('<button id="cancel">Cancel Game</button>');
 }
 
+function submitGuess(){
+    var guess = {
+        playerOneGuess: $('#input1').val(),
+        playerTwoGuess: $('#input2').val(),
+        playerThreeGuess: $('#input3').val(),
+        playerFourGuess: $('#input4').val()
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/submitGuess',
+        data: guess
+    }) .done(function(response){
+        console.log("guesses:",response);
+        checkGuesses();
+        
+    }) .fail(function(message){
+        console.log('error', message);
+        
+    });
+}
+
+
+function checkGuesses(){
+    $.ajax({
+        method: 'GET',
+        url: '/checkGuesses'
+    })
+    .done()
+    .fail()
+}
 
 
 
--- server/server.js
+++ server/server.js
@@ -2,8 +2,31 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var port = 5000;
+var randomNum = require('./modules/randomNum.js')
+
+var number = randomNum(100);
+console.log(number);
 
 app.use(express.static('server/public'));
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/submitGuess', function(req, res) {
+    console.log(req.body);
+    res.sendStatus(200);
+})
+
+app.post('/maxVal', function(req, res) {
+    console.log(req.body);
+    number = randomNum(req.body.maxVal);
+    console.log(number);
+    res.sendStatus(200);
+})
+
+
+app.get('/checkGuesses', function(req,res){
+    
+});
 
 app.listen(port, function(){
     console.log('listening on port:', port);
